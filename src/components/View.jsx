7a41321ee49b5b7a598815dd7edf291e8d4f82a5
praveen-c-s <praveencshaji123@gmail.com>
2025-07-11 15:18:36 +0530

    project creation

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,98 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Nav from './Navbar'
 import Navbar from './Navbar'
 
 const View = () => {
-  return (
-    <div>
-<Navbar/>
-<div className="container">
+    const [grocery, changeGrocery] = useState(
+        [
+        { "name": "tomato", "img": "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ7UH64K2_JbWj--JEDbWcTUfrwSvo7Xuk1tm4NYExO2VhZTWm8Qs1YdW1IctLimuJqONWxfLEUk3IIrtluNW1nDg" },
+        { "name": "banana", "img": "https://nutritionsource.hsph.harvard.edu/wp-content/uploads/2018/08/bananas-1354785_1920-1024x683.jpg" },
+        { "name": "potato", "img": "https://m.media-amazon.com/images/I/41QKCkQ2A5L.jpg" },
+        { "name": "parip", "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6N2IZKgtIoc2tGuAQ2GiMlRJxjKwzb8LShA&s" },
+        { "name": "kadala", "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKHWFyZnYu8ZkqX03PtyxeCMBo77Ge1jtf0Q&s" },
+        ]
+    )
+    return (
+        <div>
+            <Navbar />
+            <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
                         <div className="row g-3">
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            {grocery.map(
+                                (value, index) => {
+                                    return (
+                                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
-                                <div class="card" >
-                                    <img src="https://media.istockphoto.com/id/171302954/photo/groceries.jpg?s=612x612&w=0&k=20&c=D3MmhT5DafwimcYyxCYXqXMxr1W25wZnyUf4PF1RYw8=" class="card-img-top" alt="..."></img>
-                                    <div class="card-body">
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                            <div class="card" >
+                                                <img src={value.img} class="card-img-top" alt="..."></img>
+                                                <div class="card-body">
+                                                    <p class="card-text">{value.name}</p>
+                                                    <button className="btn btn-primary">add to cart</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )
+                                }
 
-                                <div class="card" >
-                                    <img src="https://media.istockphoto.com/id/171302954/photo/groceries.jpg?s=612x612&w=0&k=20&c=D3MmhT5DafwimcYyxCYXqXMxr1W25wZnyUf4PF1RYw8=" class="card-img-top" alt="..."></img>
-                                    <div class="card-body">
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div class="card" >
-                                    <img src="https://media.istockphoto.com/id/171302954/photo/groceries.jpg?s=612x612&w=0&k=20&c=D3MmhT5DafwimcYyxCYXqXMxr1W25wZnyUf4PF1RYw8=" class="card-img-top" alt="..."></img>
-                                    <div class="card-body">
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div class="card" >
-                                    <img src="https://media.istockphoto.com/id/171302954/photo/groceries.jpg?s=612x612&w=0&k=20&c=D3MmhT5DafwimcYyxCYXqXMxr1W25wZnyUf4PF1RYw8=" class="card-img-top" alt="..."></img>
-                                    <div class="card-body">
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div class="card" >
-                                    <img src="https://media.istockphoto.com/id/171302954/photo/groceries.jpg?s=612x612&w=0&k=20&c=D3MmhT5DafwimcYyxCYXqXMxr1W25wZnyUf4PF1RYw8=" class="card-img-top" alt="..."></img>
-                                    <div class="card-body">
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div class="card" >
-                                    <img src="https://media.istockphoto.com/id/171302954/photo/groceries.jpg?s=612x612&w=0&k=20&c=D3MmhT5DafwimcYyxCYXqXMxr1W25wZnyUf4PF1RYw8=" class="card-img-top" alt="..."></img>
-                                    <div class="card-body">
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div class="card" >
-                                    <img src="https://media.istockphoto.com/id/171302954/photo/groceries.jpg?s=612x612&w=0&k=20&c=D3MmhT5DafwimcYyxCYXqXMxr1W25wZnyUf4PF1RYw8=" class="card-img-top" alt="..."></img>
-                                    <div class="card-body">
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div class="card" >
-                                    <img src="https://media.istockphoto.com/id/171302954/photo/groceries.jpg?s=612x612&w=0&k=20&c=D3MmhT5DafwimcYyxCYXqXMxr1W25wZnyUf4PF1RYw8=" class="card-img-top" alt="..."></img>
-                                    <div class="card-body">
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div class="card" >
-                                    <img src="https://media.istockphoto.com/id/171302954/photo/groceries.jpg?s=612x612&w=0&k=20&c=D3MmhT5DafwimcYyxCYXqXMxr1W25wZnyUf4PF1RYw8=" class="card-img-top" alt="..."></img>
-                                    <div class="card-body">
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div class="card" >
-                                    <img src="https://media.istockphoto.com/id/171302954/photo/groceries.jpg?s=612x612&w=0&k=20&c=D3MmhT5DafwimcYyxCYXqXMxr1W25wZnyUf4PF1RYw8=" class="card-img-top" alt="..."></img>
-                                    <div class="card-body">
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                                    </div>
-                                </div>
-                            </div>
+
+                            )
+                            }
                         </div>
 
 
@@ -100,8 +46,8 @@ const View = () => {
                 </div>
             </div>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default View
